51a7b8ace62f321836519c4f01a57f961bea1cca
Saurabh Datar <sdatar2@stanford.edu>
2016-05-19 00:19:14 -0700

    Made Flask filter app

--- app.py
+++ app.py
@@ -1,7 +1,46 @@
 from flask import Flask, render_template, request
-from foo import print_record_count
+from foo import print_record_count, filter_func
 
 app = Flask(__name__)
+
+@app.route("/")
+def homepage():
+  template = 'index.html'
+  return render_template(template)
+
+@app.route("/results")
+def results():
+  reqargs = request.args
+  _score =  reqargs.get('score')
+  _risk = reqargs.get('risk')
+  _name = reqargs.get('name')
+
+  if not _score and not _risk:
+        return """
+            <h1>Error</h1>
+            <p>Must have either a score or risk value</p>
+            <p>Go <a href="{url}">back</a></p>
+        """.format(url=request.referrer)
+
+  elif request.args.get('score'):
+    search_type = 'score'
+    search_val = request.args.get('score')
+    data = filter_func(score=search_val)
+
+  elif request.args.get('risk'):
+    search_type = 'risk'
+    search_val = request.args.get('risk')
+    data = filter_func(risk=search_val)
+
+  # elif request.args.get('name'): 
+  #   search_type = 'name'
+  #   search_val = request.args.get('name')
+  #   data = filter_func(name=search_val)
+
+  template = 'results.html'
+  return render_template(template, data=data,
+               search_type=search_type,
+               search_val=search_val)     
 
 
 if __name__ == '__main__':
--- foo.py
+++ foo.py
@@ -1,7 +1,49 @@
 import csv
 
+def get_data():
+  with open('static/data/inspections.csv', 'r') as csvin:
+    datarows = list(csv.DictReader(csvin))
+  return datarows
+
 def print_record_count():
-	with open('static/data/inspections.csv', 'r') as csvin:
-		reader = list(csv.DictReader(csvin))
-	print(len(reader))
+  with open('static/data/inspections.csv', 'r') as csvin:
+    reader = list(csv.DictReader(csvin))
+  print(len(reader))
   return len(reader)
+
+def filter_by_score(score, datarows):
+  if score == 'ninety':
+    datarows = [d for d in datarows if len(d['inspection_score']) >= 2 
+                and int(d['inspection_score']) > 90]
+  elif score == 'seventy':
+    datarows = [d for d in datarows if len(d['inspection_score']) >= 2 and 
+                int(d['inspection_score']) > 70 and int(d['inspection_score']) <= 90]
+  elif score == 'sixty':
+    datarows = [d for d in datarows if len(d['inspection_score']) >= 2 and 
+                int(d['inspection_score']) > 60 and int(d['inspection_score']) <= 80]
+  elif score == 'least':
+    datarows = [d for d in datarows if len(d['inspection_score']) >= 2 and 
+                int(d['inspection_score']) <= 60]
+  return datarows
+
+def filter_by_risk(risk, datarows):
+  datarows = [d for d in datarows if risk in d['risk_category']]
+  return datarows
+
+def filter_by_name(name, datarows):
+  datarows = [d for d in datarows if name in d['business_name']]
+  return datarows
+
+
+
+def filter_func(score="", risk="", name=""):
+  matched_rows = []
+  datarows = get_data()
+
+  if score:
+    filteredrows = filter_by_score(score, datarows)
+
+  elif risk:
+    filteredrows = filter_by_risk(risk, datarows)
+
+  return filteredrows
